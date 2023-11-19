99ac4d22cc16389e59646701a8484e27ebafc957
Jyotirmoy <jyotirmoydas12@gmail.com>
2023-11-19 13:17:56 +0530

    Fixed text content of Hero slider

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,20 +1,7 @@
 import HeroBg from '../assets/hero-bg.jpg'
 import ReactSlickSliderAutoPlay from './ReactSlickSliderAutoplay'
 
-const links = [
-    { name: 'Open roles', href: '#' },
-    { name: 'Internship program', href: '#' },
-    { name: 'Our values', href: '#' },
-    { name: 'Meet our leadership', href: '#' },
-  ]
-  const stats = [
-    { name: 'Offices worldwide', value: '12' },
-    { name: 'Full-time colleagues', value: '300+' },
-    { name: 'Hours per week', value: '40' },
-    { name: 'Paid time off', value: 'Unlimited' },
-  ]
-  
-  export default function Hero() {
+export default function Hero() {
     return (
       <div className="relative isolate overflow-hidden py-0">
         {/* <img
